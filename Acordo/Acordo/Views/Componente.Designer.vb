efddadb7d8b7f13f8b8d3a258cee7e1746558e83
Maicon Ghidolin <maicon.ghidolin@gmail.com>
2017-10-13 21:15:21 -0300

    Inicio

---
+++ Acordo/Acordo/Views/Componente.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Componente
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtStatus = New System.Windows.Forms.TextBox()
+        Me.btnEnviar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtMensagem = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtStatus
+        '
+        Me.txtStatus.Enabled = False
+        Me.txtStatus.Location = New System.Drawing.Point(12, 12)
+        Me.txtStatus.Multiline = True
+        Me.txtStatus.Name = "txtStatus"
+        Me.txtStatus.Size = New System.Drawing.Size(409, 229)
+        Me.txtStatus.TabIndex = 11
+        '
+        'btnEnviar
+        '
+        Me.btnEnviar.Location = New System.Drawing.Point(346, 267)
+        Me.btnEnviar.Name = "btnEnviar"
+        Me.btnEnviar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnviar.TabIndex = 10
+        Me.btnEnviar.Text = "Enviar"
+        Me.btnEnviar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 253)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Mensagem:"
+        '
+        'txtMensagem
+        '
+        Me.txtMensagem.Location = New System.Drawing.Point(10, 269)
+        Me.txtMensagem.Name = "txtMensagem"
+        Me.txtMensagem.Size = New System.Drawing.Size(330, 20)
+        Me.txtMensagem.TabIndex = 8
+        '
+        'Componente
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 301)
+        Me.Controls.Add(Me.txtStatus)
+        Me.Controls.Add(Me.btnEnviar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtMensagem)
+        Me.Name = "Componente"
+        Me.Text = "Componente"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtStatus As TextBox
+    Friend WithEvents btnEnviar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtMensagem As TextBox
+End Class
---
+++ Acordo/Acordo/Views/Componente.vb
@@ -0,0 +1,142 @@
+﻿Imports System.Net.Sockets
+Imports System.Net
+Imports System.Text
+Imports System.Web.Script.Serialization
+
+Public Class Componente
+
+#Region "Variaveis"
+
+    Private _socketTcpCliente As TcpClient
+    Private _porta As Integer = 8000
+    Private _IP As IPAddress = IPAddress.Parse("127.0.0.1")
+    Private _networkStream As NetworkStream
+    Private _serializer As JavaScriptSerializer
+    Private _localVirtualTime As Integer
+
+    Delegate Sub SetStatusCallback([text] As String)
+
+#End Region
+
+#Region "Eventos"
+
+    Private Sub Componente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            _serializer = New JavaScriptSerializer
+            _localVirtualTime = 0
+            _socketTcpCliente = New TcpClient()
+            SetStatus("Estabelecendo conexão.")
+            'cria um socket para este ip e porta
+            _socketTcpCliente.Connect(_IP, _porta)
+            'pega o stream entre o cliente e o servidor neste ip e porta
+            _networkStream = _socketTcpCliente.GetStream()
+
+            Dim dto As Models.DTO = LeMensagem()
+
+            Me.Text = "Cliente " & dto.mensagem
+            SetStatus("Conexão aceita...")
+
+            _localVirtualTime = Math.Max(_localVirtualTime, dto.timeStamp)
+
+            Dim ctThread As Threading.Thread = New Threading.Thread(AddressOf OuveRetornoServer)
+            ctThread.Start()
+        Catch ex As Exception
+            'se n encontrou um servidor mostra uma mensagem e fecha o form
+            Dim result = MessageBox.Show("Ocorreu um erro ao se conectar com o servidor!" & vbCrLf & ex.Message, "Atenção", MessageBoxButtons.OK)
+            If result = DialogResult.OK Then
+                Me.Close()
+            End If
+        End Try
+    End Sub
+
+    Private Sub Componente_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        _socketTcpCliente.Close()
+        Environment.Exit(0)
+    End Sub
+
+    Private Sub txtMensagem_KeyDown(sender As Object, e As KeyEventArgs) Handles txtMensagem.KeyDown
+        If e.KeyCode = Keys.KeyCode.Enter Then
+            btnEnviar.PerformClick()
+        End If
+    End Sub
+
+    Private Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
+        'verifica se pode ler e escrever no stream
+        If _networkStream.CanWrite Then
+            EnviaMensagem(txtMensagem.Text)
+            txtMensagem.Text = ""
+        Else
+            SetStatus("Não é possivel escrever dados neste stream")
+            _socketTcpCliente.Close()
+        End If
+    End Sub
+
+#End Region
+
+#Region "Metodos cliente"
+
+
+
+#End Region
+
+#Region "Metodos servidor"
+
+
+
+#End Region
+
+#Region "Metodos genericos"
+
+    Private Sub SetStatus(ByVal text As String)
+        If Me.txtStatus.InvokeRequired Then
+            Dim d As New SetStatusCallback(AddressOf SetStatus)
+            Me.Invoke(d, New Object() {text})
+        Else
+            Dim texto = ""
+            If (Not String.IsNullOrEmpty(Me.txtStatus.Text)) Then
+                texto &= vbCrLf
+            End If
+            texto &= text
+            Me.txtStatus.AppendText(texto)
+        End If
+    End Sub
+
+    Private Sub EnviaMensagem(ByVal mensagem As String)
+        _localVirtualTime += 1
+        ' cria objeto para mandar
+        Dim dto As New Models.DTO
+        dto.timeStamp = _localVirtualTime
+        dto.mensagem = mensagem
+        'serializa objeto
+        Dim resultado As String = _serializer.Serialize(dto)
+        Dim sendBytes As [Byte]() = Encoding.UTF8.GetBytes(resultado)
+        _networkStream.Write(sendBytes, 0, sendBytes.Length)
+    End Sub
+
+    Private Function LeMensagem() As Models.DTO
+        Dim bytes(_socketTcpCliente.ReceiveBufferSize) As Byte
+        _networkStream.Read(bytes, 0, CInt(_socketTcpCliente.ReceiveBufferSize))
+        Dim returndata As String = Encoding.UTF8.GetString(bytes)
+        Dim dto As Models.DTO = _serializer.Deserialize(Of Models.DTO)(returndata.Replace(vbNullChar, ""))
+        Return dto
+    End Function
+
+#End Region
+
+
+    Private Sub OuveRetornoServer()
+        While True
+            If _NetworkStream.CanRead Then
+                Dim dto As Models.DTO = LeMensagem()
+                _localVirtualTime = Math.Max(_localVirtualTime, dto.timeStamp)
+                SetStatus("[" & dto.timeStamp & "] " & dto.mensagem)
+            Else
+                SetStatus("Não é possivel ler dados deste stream")
+                _SocketTcpCliente.Close()
+            End If
+        End While
+    End Sub
+
+
+
+End Class
